226fe4d6610dd2159e4a23d5a86807c395bb6b84
赛灵思中文学习资料和开源设计 <xup.shanghai@gmail.com>
2021-04-28 16:33:01 +0800

    Update tcl_script.tcl

--- problems/fft/tcl_script.tcl
+++ problems/fft/tcl_script.tcl
@@ -1,8 +1,8 @@
 open_project fft
 set_top fft
 add_files fft.cpp
-add_files -tb out.gold.dat -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
-add_files -tb fft_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb out.gold.dat -cflags "-I."
+add_files -tb fft_test.cpp -cflags "-I."
 open_solution "solution1" -flow_target vitis
 set_part {xcu50-fsvh2104-2-e}
 create_clock -period 10 -name default
