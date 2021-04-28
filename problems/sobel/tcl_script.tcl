8e3da2ff183a16c1f22231d0abc5093d43aeb0a6
赛灵思中文学习资料和开源设计 <xup.shanghai@gmail.com>
2021-04-28 16:33:32 +0800

    Update tcl_script.tcl

--- problems/sobel/tcl_script.tcl
+++ problems/sobel/tcl_script.tcl
@@ -1,9 +1,9 @@
 open_project sobel
 set_top sobel
 add_files sobel.cpp
-add_files -tb data.txt -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
-add_files -tb dst.txt -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
-add_files -tb sobel_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb data.txt -cflags "-I."
+add_files -tb dst.txt -cflags "-I."
+add_files -tb sobel_test.cpp -cflags "-I."
 open_solution "solution1" -flow_target vitis
 set_part {xcu50-fsvh2104-2-e}
 create_clock -period 10 -name default
