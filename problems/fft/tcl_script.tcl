35c9c3a1ceef4929e9fbde76b26ffa75def5be94
赛灵思中文学习资料和开源设计 <xup.shanghai@gmail.com>
2021-05-27 02:05:38 +0800

    Update tcl_script.tcl

--- problems/fft/tcl_script.tcl
+++ problems/fft/tcl_script.tcl
@@ -3,7 +3,7 @@ set_top fft
 add_files fft.cpp
 add_files -tb out.gold.dat -cflags "-I."
 add_files -tb fft_test.cpp -cflags "-I."
-open_solution "solution1" -flow_target vitis
+open_solution "solution1" -flow_target vivado
 set_part {xcu50-fsvh2104-2-e}
 create_clock -period 10 -name default
 csim_design
