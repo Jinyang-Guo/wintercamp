e0efc02128f4db26ad47e7b010cd2f95e9b06e8a
赛灵思中文学习资料和开源设计 <xup.shanghai@gmail.com>
2021-05-27 02:06:12 +0800

    Update tcl_script.tcl

--- problems/sobel/tcl_script.tcl
+++ problems/sobel/tcl_script.tcl
@@ -4,7 +4,7 @@ add_files sobel.cpp
 add_files -tb data.txt -cflags "-I."
 add_files -tb dst.txt -cflags "-I."
 add_files -tb sobel_test.cpp -cflags "-I."
-open_solution "solution1" -flow_target vitis
+open_solution "solution1" -flow_target vivado
 set_part {xcu50-fsvh2104-2-e}
 create_clock -period 10 -name default
 csim_design
